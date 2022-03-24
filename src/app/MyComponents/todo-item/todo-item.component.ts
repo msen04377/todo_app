7670c887fcca4e35756d5087e16a473b85127df0
Husain Saify <hello@hackerkernel.com>
2022-03-25 01:08:24 +0530

    todo app angular

---
+++ src/app/MyComponents/todo-item/todo-item.component.ts
@@ -0,0 +1,23 @@
+import { Component, Input, OnInit, Output ,EventEmitter} from '@angular/core';
+import { Todo } from 'src/app/Todo';
+
+@Component({
+  selector: 'app-todo-item',
+  templateUrl: './todo-item.component.html',
+  styleUrls: ['./todo-item.component.css']
+})
+export class TodoItemComponent implements OnInit {
+
+  @Input() todo: Todo | undefined;
+  @Output() todoDelete: EventEmitter<Todo> = new EventEmitter()
+  constructor() {
+
+   }
+  ngOnInit(): void {
+  }
+  onClick(todo:any){
+    this.todoDelete.emit(todo);
+    console.log("data * ",todo)
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodosComponent } from './MyComponents/todos/todos.component';
+import { TodoItemComponent } from './MyComponents/todo-item/todo-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    // make all component declare here
+    AppComponent,
+    TodosComponent,
+    TodoItemComponent
   ],
   imports: [
+    // external module declare here
     BrowserModule,
     AppRoutingModule
   ],
